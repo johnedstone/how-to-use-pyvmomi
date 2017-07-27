3e001317213163b9012c6889388468efbda28a6c
johnedstone <johnedstone@gmail.com>
2017-07-27 16:26:27 -0400

    job control update, ready for real job

--- pyvmomi_restapi/vm_helpers/manage_cdrom.py
+++ pyvmomi_restapi/vm_helpers/manage_cdrom.py
@@ -1,12 +1,50 @@
 # -*- coding: utf-8 -*-
+import logging, shlex, subprocess
+import jsonpickle
+from threading import Timer
+
+from django.conf import settings
+
+logger = logging.getLogger(settings.PROJECT_LOGGING)
 
 def manage_cdrom(obj):
-    result = {
-        'ok': True,
-        'status_code': 0,
-        'stdout': 'VM change',
-    }
+    """ https://www.blog.pythonlibrary.org/2016/05/17/python-101-how-to-timeout-a-subprocess/
 
+    Waiting for: Python 3.5 added the run function which accepts a timeout parameter. 
+    """
+  
+    kill = lambda process: process.kill()
+    cmd = 'echo boo'
+
+    class Result(object):
+
+       def __init__(self):
+           self.ok = False
+           self.stdout = ''
+           self.stderr = ''
+           self.returncode = 99 
+        
+
+    result = Result()
+
+    p1 = subprocess.Popen(shlex.split(cmd),
+                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    my_timer = Timer(30, kill, [p1])
+
+    try:
+        my_timer.start()
+        result.stdout, result.stderr = p1.communicate()
+        result.returncode =  p1.returncode
+    finally:
+        my_timer.cancel()
+
+    if result.returncode == 0:
+        result.ok = True
+
+    if result.returncode == -9:
+        result.stderr += 'Call to cdrom killed by the Timer'
+
+    logger.info(jsonpickle.encode(result))
     return result
 
 # vim: ai et ts=4 sw=4 sts=4 nu ru
--- pyvmomi_restapi/vm_helpers/models.py
+++ pyvmomi_restapi/vm_helpers/models.py
@@ -32,10 +32,10 @@ class ManageCdrom(TimeStampModel):
     state = models.CharField(choices=STATE_CHOICES, max_length=25)
     iso_path = models.CharField(max_length=200, help_text='[datastore] path/to/iso', blank=True )
     
+    returncode = models.IntegerField(default=999, help_text='No need to edit')
+    status = models.CharField(max_length=20, default='pending', help_text='No need to edit')
     stdout = models.TextField(max_length=100, default='', help_text='No need to edit')
     stderr = models.TextField(max_length=100, default='', help_text='No need to edit')
-    status_code = models.IntegerField(default=99, help_text='No need to edit')
-    status = models.CharField(max_length=20, default='pending', help_text='No need to edit')
 
     class Meta:
         ordering = ['vmname']
@@ -47,21 +47,20 @@ class ManageCdrom(TimeStampModel):
         try:
             logger.info('vmname: {}'.format(self.vmname))
             result = manage_cdrom(self)
-            if result and result['ok']:
-                pass #self.status_code = result.status_code
-#                self.stdout = result.json()
-#                self.status = "Finished"
+
+            self.returncode = result.returncode
+            self.stdout = result.stdout
+            self.stderr = result.stderr
+
+            if result.ok:
+                self.status = "Finished"
             else:
-                pass#self.status_code = 'unknown'
-#                self.stdout = 'unknown'
-#                self.status = 'unknown'
+                self.status = 'Failed'
         except Exception as e:
             self.stderr = '{}'.format(e)
-            self.status = 'failed'
-            raise(e)
-#        finally:
-#            pass
-#
-        super(ManageCdrom, self).save(*args, **kwargs)
+            self.status = 'Failed: Exception thrown'
+
+        finally:
+            return super(ManageCdrom, self).save(*args, **kwargs)
 
 # vim: ai et ts=4 sw=4 sts=4 nu ru
