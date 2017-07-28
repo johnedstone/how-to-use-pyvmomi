eeb91c049eb6a9e25264913888a618c850368e8c
johnedstone <johnedstone@gmail.com>
2017-07-28 19:03:14 -0400

    update urls

--- pyvmomi_restapi/pyvmomi_restapi/urls.py
+++ pyvmomi_restapi/pyvmomi_restapi/urls.py
@@ -9,8 +9,8 @@ from vm_helpers.urls import router as vm_helpers_router
 schema_view = get_swagger_view(title='IIHS INFRASTRUCTURE REST API')
 
 urlpatterns = [  
-    url(r'^$', lambda x: redirect('/api/', permanent=False), name='home'),
-    url(r'^accounts', lambda x: redirect('/api/', permanent=False), name='home'),
+    url(r'^$', lambda x: redirect('/api/', permanent=False), name='api-home'),
+    url(r'^accounts', lambda x: redirect('/swagger/', permanent=False), name='accounts'),
     url(r'^swagger/$', schema_view),
     url(r'^api/', include(vm_helpers_router.urls)),
     url(r'^liveness/', lambda request:HttpResponse(status=200)),
