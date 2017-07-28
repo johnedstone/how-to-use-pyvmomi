e977ff3ef9f0ed7a2160fcd683babe8d7a7dbfaa
johnedstone <johnedstone@gmail.com>
2017-07-28 17:55:14 -0400

    settings update

--- pyvmomi_restapi/pyvmomi_restapi/settings.py
+++ pyvmomi_restapi/pyvmomi_restapi/settings.py
@@ -133,11 +133,11 @@ if DEBUG:
 else:
     PROJECT_LOGGING = 'info_logger'
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework.authentication.TokenAuthentication',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 CORS_ORIGIN_ALLOW_ALL = True
 
